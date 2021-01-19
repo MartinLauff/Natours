770e39d172045479b3640e74890e7d985de1bf4d
Martin Lauff <lauff1234@gmail.com>
2021-01-19 21:40:53 +0100

    dwwd

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -9,6 +9,7 @@ exports.alerts = (req, res, next) => {
   if (alert === 'booking')
     res.locals.alert =
       "Your booking was succesful! Please check your email for confirmation. If your booking doesn't show up here immediately, please come back later.";
+  next();
 };
 
 exports.getOverview = catchAsync(async (req, res, next) => {
